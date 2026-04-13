883d27f22249696326404088b4af9d2aa4b47a56
Prakhar Singh <147630079+prakh73ar@users.noreply.github.com>
2026-04-13 04:26:15 +0000

    variables and dataType

---
+++ Basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId=144553
+let accountEmail="shaab@gmail.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+
+let accountState;
+
+//accountId=2
+
+accountEmail="hc@gmail.com"
+
+accountPassword="212121"
+
+accountCity="Lucknow"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
---
+++ Basics/02_datatype.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all the JS code as newer version
+
+//alert(3+3) we are using node not browser
+
+console.log(3 + 3);
+
+console.log("Hitesh")
+
+
+let name="Prakhar";
+let age=18;
+let isLoggedIn=false;
+
+//number
+//bigint
+//String
+//boolean
+//null
+//undefined
+//symbol
+
+//object
+
+console.log(typeof undefined);
+
